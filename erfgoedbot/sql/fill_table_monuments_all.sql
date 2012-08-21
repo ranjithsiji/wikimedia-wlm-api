54aca6109f3cf1919045cecc6a8d99c869d582b1
Max Semenik <maxsem.wiki@gmail.com>
2012-08-22 01:35:41 +0400

    Update to r876
    ------------------------------------------------------------------------
    r876 | maxsem | 2012-08-22 01:31:08 +0400 | 2 lines
    
    * Allow smart filtering when no adm0 has been provided
    * Working getParam() caching
    ------------------------------------------------------------------------
    r875 | maxsem | 2012-08-22 00:04:18 +0400 | 1 line
    
    Bug 39540 - 'lat' and 'lon' are 0 when the value is NULL in the database JSON output
    ------------------------------------------------------------------------
    r874 | maxsem | 2012-08-21 22:00:05 +0400 | 1 line
    
    Logging refactoring
    ------------------------------------------------------------------------
    r873 | platonides | 2012-08-21 21:15:42 +0400 | 2 lines
    
    Add Panama to fill_table_monuments_all.sql
    
    ------------------------------------------------------------------------
    r872 | platonides | 2012-08-21 20:54:26 +0400 | 3 lines
    
    Extra field for Panama, used for a related article
    but not one about the monument (eg. about the city).
    
    ------------------------------------------------------------------------
    r871 | platonides | 2012-08-21 20:46:14 +0400 | 2 lines
    
    (Bug 38327) Add Panama in Spanish
    
    ------------------------------------------------------------------------
    r870 | multichill | 2012-08-21 20:44:23 +0400 | 1 line
    
    Descrease admin sizes
    ------------------------------------------------------------------------
    r869 | multichill | 2012-08-21 20:41:16 +0400 | 1 line
    
    Fix Russia, looks like they moved from namespace 4 to 104
    ------------------------------------------------------------------------
    r868 | platonides | 2012-08-21 20:29:03 +0400 | 2 lines
    
    Move Ghana to be alphabetically sorted inside the file.
    
    ------------------------------------------------------------------------
    r867 | maxsem | 2012-08-21 19:36:41 +0400 | 1 line
    
    Fix language codes
    ------------------------------------------------------------------------
    r866 | maxsem | 2012-08-21 03:18:57 +0400 | 1 line
    
    Sort admin structure by name
    ------------------------------------------------------------------------
    
    Change-Id: I13878a383965f2dea01dd7e4a72a8e14518a3200

--- erfgoedbot/sql/fill_table_monuments_all.sql
+++ erfgoedbot/sql/fill_table_monuments_all.sql
@@ -16,11 +16,11 @@ CREATE TABLE `monuments_all_tmp` (
   `country` varchar(10) NOT NULL DEFAULT '',
   `lang` varchar(10) NOT NULL DEFAULT '',
   `id` varchar(25) NOT NULL DEFAULT '0',
-  `adm0` varchar(5) NOT NULL DEFAULT '',
-  `adm1` varchar(10) DEFAULT NULL,
-  `adm2` varchar(255) DEFAULT NULL,
-  `adm3` varchar(255) DEFAULT NULL,
-  `adm4` varchar(255) DEFAULT NULL,
+  `adm0` varchar(3) NOT NULL DEFAULT '',
+  `adm1` varchar(7) DEFAULT NULL,
+  `adm2` varchar(100) DEFAULT NULL,
+  `adm3` varchar(150) DEFAULT NULL,
+  `adm4` varchar(200) DEFAULT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `address` varchar(255) NOT NULL DEFAULT '',
   `municipality` varchar(255) NOT NULL DEFAULT '',
@@ -1089,6 +1089,29 @@ SELECT 'no' AS `country`,
         `monument_article` AS `monument_article`,
         `registrant_url` AS `registrant_url`
 	FROM `monuments_no_(no)`;
+/* Panamá */
+REPLACE INTO `monuments_all_tmp` (`country`, `lang`, `id`, `adm0`, `adm1`, `adm2`, `adm3`, `adm4`, `name`, `address`, `municipality`, `lat`,  `lon`, `lat_int`,  `lon_int`,  `image`,  `source`, `changed`, `monument_article`, `registrant_url` )
+SELECT 'pa' AS `country`,
+       'es' AS `lang`,
+		`id` AS `id`, 
+		'pa' AS `adm0`,
+		NULL AS `adm1`,
+		NULL AS `adm2`,
+		NULL AS `adm3`,
+		NULL AS `adm4`,
+	`nombre` AS `name`,
+	`dirección` AS `address`,
+	`provincia` AS `municipality`,
+	`lat` AS `lat`,
+	`lon` AS `lon`,
+	ROUND(`lat` * @granularity) AS `lat_int`,
+	ROUND(`lon` * @granularity) AS `lon_int`,
+	`imagen` AS `image`,
+	`source` AS `source`,
+	`changed` AS `changed`,
+    `monument_article` AS `monument_article`,
+    '' AS `registrant_url`
+	FROM `monuments_pl_(pl)`;
 /* Poland */
 REPLACE INTO `monuments_all_tmp` (`country`, `lang`, `id`, `adm0`, `adm1`, `adm2`, `adm3`, `adm4`, `name`, `address`, `municipality`, `lat`,  `lon`, `lat_int`,  `lon_int`,  `image`,  `source`, `changed`, `monument_article`, `registrant_url` )
 SELECT 'pl' AS `country`,
